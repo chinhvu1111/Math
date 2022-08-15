69a52112b6be73ceb235d228fc2f39194d359237
chinhvu1111 <ctybpphong@gmail.com>
2022-08-16 01:59:30 +0700

    Update exam 115

--- Algorithm/src/main/java/interviews/E106_MinimizedMaximumOfProductsDistributedToAnyStore.java
+++ Algorithm/src/main/java/interviews/E106_MinimizedMaximumOfProductsDistributedToAnyStore.java
@@ -3,10 +3,27 @@ package interviews;
 public class E106_MinimizedMaximumOfProductsDistributedToAnyStore {
 
     public static int minimizedMaximum(int n, int[] quantities) {
+        int low=0;
+        int high=0;
+        int length=quantities.length;
+        long sum=0;
+
+        for(int i=0;i<length;i++){
+            high=Math.max(high, quantities[i]);
+            sum+=quantities[i];
+        }
+        low=(int)(sum/n);
+
+        while (low<=high){
+            int mid=low + (high-low)/2;
+
+
+        }
+
         return 1;
     }
 
     public static void main(String[] args) {
-
+        int[] quantities=new int[]{15,10,10};
     }
 }
