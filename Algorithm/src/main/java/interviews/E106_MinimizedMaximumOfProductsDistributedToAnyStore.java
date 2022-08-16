47c45eabf0657ab5fe157c276f73e344bf8c6263
chinhvu1111 <ctybpphong@gmail.com>
2022-08-17 02:02:00 +0700

    Update exam 115

--- Algorithm/src/main/java/interviews/E106_MinimizedMaximumOfProductsDistributedToAnyStore.java
+++ Algorithm/src/main/java/interviews/E106_MinimizedMaximumOfProductsDistributedToAnyStore.java
@@ -3,27 +3,50 @@ package interviews;
 public class E106_MinimizedMaximumOfProductsDistributedToAnyStore {
 
     public static int minimizedMaximum(int n, int[] quantities) {
-        int low=0;
+        int low=Integer.MAX_VALUE;
         int high=0;
         int length=quantities.length;
-        long sum=0;
+
+        if(n==1&&length==1){
+            return quantities[0];
+        }
 
         for(int i=0;i<length;i++){
             high=Math.max(high, quantities[i]);
-            sum+=quantities[i];
+            low=Math.min(low,quantities[i]);
         }
-        low=(int)(sum/n);
+        int rs=0;
 
         while (low<=high){
             int mid=low + (high-low)/2;
+            int count=0;
 
-
+            for(int i=0;i<length;i++){
+                count+=(quantities[i]+mid-1)/mid;
+            }
+            if(count>n){
+                //1, Bài này nếu để mid trên đây sẽ bị sai
+                //2, chỗ này nếu gán low=mid ==> Sẽ bị loop vô tận
+                //==> Cần nhớ rõ tại sao
+                low=mid+1;
+            }else {
+                rs=mid;
+                high=mid-1;
+            }
         }
 
-        return 1;
+        return rs;
     }
 
     public static void main(String[] args) {
-        int[] quantities=new int[]{15,10,10};
+//        int[] quantities=new int[]{15,10,10};
+//        int n=7;
+        int[] quantities=new int[]{11,6};
+        int n=6;
+//        int[] quantities=new int[]{100000};
+//        int n=1;
+//        int[] quantities=new int[]{5,7};
+//        int n=2;
+        System.out.println(minimizedMaximum(n, quantities));
     }
 }
