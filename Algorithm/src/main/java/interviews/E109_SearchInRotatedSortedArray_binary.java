20e0d234605f256e8dcf2b43974e5453a2a775b6
chinhvu1111 <ctybpphong@gmail.com>
2022-08-24 02:18:01 +0700

    Update refactor new exercises 106

--- Algorithm/src/main/java/interviews/E109_SearchInRotatedSortedArray_binary.java
+++ Algorithm/src/main/java/interviews/E109_SearchInRotatedSortedArray_binary.java
@@ -1,6 +1,7 @@
 package interviews;
 
 public class E109_SearchInRotatedSortedArray_binary {
+
     public static int search(int[] nums, int target) {
         int index=findIndex(nums, target, 0, nums.length-1);
 
@@ -59,6 +60,47 @@ public class E109_SearchInRotatedSortedArray_binary {
         return rs;
     }
 
+    public static int searchOptimize(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+
+        //[4],5,(6),7,0,1,[2]
+        //[4],5,(6)
+        //[6],(4),[5] ==> cái này low=mid+1
+        //---> Không cần nums[mid]> nums[low]
+        //---> Không cần nums[mid]> nums[high]
+        while(low<high){
+            int mid=low + (high-low)/2;
+
+            if(nums[mid]>nums[high]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        int indexSmallest=low;
+        low=0;
+        high=nums.length-1;
+
+        while (low<high){
+            int mid=low + (high-low)/2;
+
+            if(nums[mid]>=target){
+                if(mid<=indexSmallest){
+                    high=mid;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(mid<indexSmallest){
+                    high=mid-1;
+                }else{
+                    low=mid;
+                }
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
 //        int arr[]=new int[]{4,5,6,7,0,1,2};
 //        int target=0;
@@ -82,13 +124,13 @@ public class E109_SearchInRotatedSortedArray_binary {
         //Case : Lỗi khi 8 nằm trong (low, high) ===> Mình bị sai 1 chút ở case cuối cùng
 //        int arr[]=new int[]{4,5,6,7,8,1,2,3};
 //        int target=8;
-//        int arr[]=new int[]{4,5,6,7,8,1,2,3};
-//        int target=8;
+        int arr[]=new int[]{4,5,6,7,8,1,2,3};
+        int target=8;
         //Case : Lỗi ở predicate cuối cùng
         //if((left<=value&&value>=nums[mid])||(left>=value&&value<=nums[mid])){
         // high=mid;
-        int arr[]=new int[]{5,1,2,3,4};
-        int target=1;
+//        int arr[]=new int[]{5,1,2,3,4};
+//        int target=1;
         System.out.println(search(arr, target));
         //Bài này tư duy như sau:
         //1, Ở đây ta dùng custom binary search + break conditions để scan hết các điều kiện
@@ -119,5 +161,11 @@ public class E109_SearchInRotatedSortedArray_binary {
         //+ left <= midVal ( 0 ... 1)
         //+ value >= left (7 ... 0)
         //+ left >= midVal <= right
+        //1.3, Chú ý 1 số trường hợp sẽ return -1 (LUÔN) nếu không có cases nào trong (low --> high) thỏa mãn
+        //
+        //
+        //2, Ta sẽ tối giản bài toán bên trên:
+        //
+        System.out.println(searchOptimize(arr, target));
     }
 }
