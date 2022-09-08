c91d32ccacbfe9f872981ec442e7b125c6592008
chinhvu1111 <ctybpphong@gmail.com>
2022-09-09 01:31:15 +0700

    Update refactor new exercises 106

--- Algorithm/src/main/java/interviews/E150_TaskScheduler.java
+++ Algorithm/src/main/java/interviews/E150_TaskScheduler.java
@@ -6,6 +6,7 @@ public class E150_TaskScheduler {
 
     static class Node{
         int x;
+        int currentPosition=-1;
         char c;
 
         public Node(int x, char c) {
@@ -32,16 +33,74 @@ public class E150_TaskScheduler {
             Node node=new Node(entry.getValue(), entry.getKey());
             queue.add(node);
         }
-        while (!queue.isEmpty()){
+        List<Character> rs=new ArrayList<>();
+        int index=0;
 
+        while (!queue.isEmpty()){
+            List<Node> temp=new ArrayList<>();
+
+            while (!queue.isEmpty()){
+                Node currentNode=queue.poll();
+
+                if(currentNode.currentPosition!=-1&&(index-currentNode.currentPosition<n+1)){
+//                    System.out.printf("%s %s %s %s, ", index, currentNode.c, currentNode.currentPosition, currentNode.x);
+                    temp.add(currentNode);
+                    continue;
+                }
+                currentNode.x--;
+                currentNode.currentPosition=index;
+//                System.out.printf("%s %s %s, ", currentNode.c, currentNode.currentPosition, currentNode.x);
+
+                if(currentNode.x>0){
+                    temp.add(currentNode);
+                }
+                rs.add(currentNode.c);
+                break;
+            }
+            index++;
+            queue.addAll(temp);
         }
 
-        return 1;
+        return index;
+    }
+
+    public static int leastIntervalOptimize(char[] tasks, int n) {
+        int[] count =new int[27];
+        int length=tasks.length;
+
+        for(int i=0;i<length;i++){
+            count[tasks[i]-'A']++;
+        }
+        int rs=0;
+        int t=0;
+        PriorityQueue<Integer> integers=new PriorityQueue<>();
+
+        for(int i=0;i<=26;i++){
+            if(count[i]!=0){
+                integers.add(count[i]);
+//                System.out.printf("%s, ", count[i]);
+            }
+        }
+//        System.out.println();
+        while (!integers.isEmpty()){
+            Integer currentValue=integers.poll();
+            rs=Math.max(rs, (currentValue-1)*(n+1)+1+t);
+//            System.out.printf("%s, ", (currentValue-1)*(n+1)+1+t);
+            t++;
+        }
+
+        return rs;
     }
 
     public static void main(String[] args) {
-        String[] s=new String[]{"A","A","A","A","A","A","B","C","D","E","F","G"};
-        int n=2;
-
+//        char[] s=new char[]{'A','A','A','A','A','A','B','C','D','E','F','G'};
+//        char[] s=new char[]{'A','A','A','B','B','B'};
+        char[] s=new char[]{'A','A','A','B','B','B'};
+//        char[] s=new char[]{'A'};
+//        char[] s=new char[]{'A','A','A'};
+        int n=0;
+        System.out.println(leastInterval(s, n));
+        System.out.println(leastIntervalOptimize(s, n));
+        //
     }
 }
