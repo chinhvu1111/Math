aaae9c7b8ecff67c7b5b1ace9018a9584ea5fb98
chinhvu <chinhvu@chinhvus-MacBook-Pro.local>
2022-12-23 02:53:47 +0700

    Update code

---
+++ Algorithm/src/main/java/interviews/E238_MostFrequentNumberFollowingKeyInAnArray.java
@@ -0,0 +1,10 @@
+package interviews;
+
+public class E238_MostFrequentNumberFollowingKeyInAnArray {
+    public static int mostFrequent(int[] nums, int key) {
+        return 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
